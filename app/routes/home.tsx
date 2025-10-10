01041fc5bc7d3e70cb2457719da6c2e7f16445c6
tenzing <tenam2000@hotmail.com>
2025-10-10 17:58:11 -0400

    second commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,36 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "constants";
+import ResumeCard from "~/components/ResumeCard";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ProfileIQ" },
+    { name: "description", content: "Optimize your Resume and get hired!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1 className="section-title">Optimize your Resume and Track your Application</h1>
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        </div>
+      </section>
+      {resumes.length > 0 && (
+         <div className="resumes-section">
+          {resumes.map((resume) =>(
+          <ResumeCard key={resume.id} resume={resume} />
+          ))}
+         </div>
+      )}
+     
+
+     
+    </main>
+  );
 }
