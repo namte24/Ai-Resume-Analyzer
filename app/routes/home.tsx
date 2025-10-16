d8c74a73a48d149cae8650cf185571a08006bc37
tenzing <tenam2000@hotmail.com>
2025-10-16 18:47:12 -0400

    Authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,10 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { resumes } from "constants";
+import  {resumes}  from "constants/index";
 import ResumeCard from "~/components/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
+import { useLocation, useNavigate } from "react-router";
+import { useEffect } from "react";
 
 
 export function meta({}: Route.MetaArgs) {
@@ -12,23 +15,31 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const {auth} = usePuterStore()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if(!auth.isAuthenticated) {
+            navigate('/auth?next=/')
+        }
+    },[auth.isAuthenticated])
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
+       
       <section className="main-section">
-        <div className="page-heading">
+        <div className="page-heading py-16">
           <h1 className="section-title">Optimize your Resume and Track your Application</h1>
           <h2>Review your submissions and check AI-powered feedback.</h2>
         </div>
-      </section>
+      
       {resumes.length > 0 && (
          <div className="resumes-section">
           {resumes.map((resume) =>(
           <ResumeCard key={resume.id} resume={resume} />
           ))}
-         </div>
-      )}
-     
+         </div> )}
+     </section>
 
      
     </main>
